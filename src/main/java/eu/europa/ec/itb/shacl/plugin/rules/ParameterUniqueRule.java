c6a83407c684ffac1792c4d4af59897754e023f6
Maria <mfontsan@everis.com>
2020-07-24 12:14:59 +0200

    Implementation Rule ID: parameter-name-unique

---
+++ src/main/java/eu/europa/ec/itb/shacl/plugin/rules/ParameterUniqueRule.java
@@ -0,0 +1,65 @@
+package eu.europa.ec.itb.shacl.plugin.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+
+import eu.europa.ec.itb.shacl.plugin.Report;
+import eu.europa.ec.itb.shacl.plugin.Rules;
+import eu.europa.ec.itb.shacl.plugin.utils.JenaModelUtils;
+
+/**
+ * Rule ID: parameter-name-unique
+ * Rule Definition: https://www.w3.org/TR/shacl/#syntax-rule-parameter-name-unique
+ * Rule Description: A constraint component where two or more parameter declarations use the same parameter names is ill-formed.
+ * @author mfontsan
+ *
+ */
+public class ParameterUniqueRule extends JenaModelUtils  implements Rules {
+	private static String parameterProperty = shaclNamespace + "parameter";
+	private static String pathProperty = shaclNamespace + "path";
+	
+	private String ruleDescription = "A constraint component where two or more parameter declarations use the same parameter names is ill-formed.";
+	private static String reportAssertionID = "http://www.w3.org/ns/shacl#path";
+	
+	public ParameterUniqueRule(Model currentModel, Report report) {		
+		super(currentModel, report);
+	}
+
+
+	public void validateRule() {
+		Property pParameter = this.currentModel.getProperty(parameterProperty);
+		Property pPath = this.currentModel.getProperty(pathProperty);
+		List<Resource> subjectsParameter = this.currentModel.listSubjectsWithProperty(pParameter).toList();
+		
+		for(Resource subject : subjectsParameter) {			
+			List<RDFNode> objectParameter = this.currentModel.listObjectsOfProperty(subject, pParameter).toList();
+			List<String> pathValues = new ArrayList<>();
+			List<String> pathDuplicates = new ArrayList<>();
+			
+			for(RDFNode object : objectParameter) {
+				if(object.isResource()) {
+					Statement st = object.asResource().getProperty(pPath);
+					
+					if(!pathValues.contains(st.getObject().toString())) {
+						pathValues.add(st.getObject().toString());						
+					}else {
+						pathDuplicates.add(st.getObject().toString());
+					}
+				}
+			}
+			
+			for(String duplicate : pathDuplicates) {
+				report.setErrors(1);    				
+				report.setErrorItem(ruleDescription, reportAssertionID, subject.toString(), null, duplicate);				
+			}
+		}
+
+	}
+
+}
