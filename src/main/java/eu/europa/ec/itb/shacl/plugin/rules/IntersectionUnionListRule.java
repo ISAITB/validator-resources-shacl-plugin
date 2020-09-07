a71b341f9cd6dc3b69d7860563277162cc6aee17
Maria <mfontsan@everis.com>
2020-09-07 15:36:55 +0200

    Implementation Rule ID: Intersection and Union

--- src/main/java/eu/europa/ec/itb/shacl/plugin/rules/IntersectionUnionListRule.java
+++ src/main/java/eu/europa/ec/itb/shacl/plugin/rules/IntersectionUnionListRule.java
@@ -1,6 +1,10 @@
 package eu.europa.ec.itb.shacl.plugin.rules;
 
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFList;
+import org.apache.jena.rdf.model.RDFNode;
 
 import eu.europa.ec.itb.shacl.plugin.Report;
 import eu.europa.ec.itb.shacl.plugin.Rules;
@@ -14,7 +18,8 @@ import eu.europa.ec.itb.shacl.plugin.utils.JenaModelUtils;
  *
  */
 public class IntersectionUnionListRule extends JenaModelUtils  implements Rules {	
-	private static String reportAssertionID = "http://www.w3.org/ns/shacl#";
+	private static String unionProperty = shaclNamespace + "union";
+	private static String intersectionProperty = shaclNamespace + "intersection";
 	
 	private String ruleDescriptionIntersection = "Intersection expression is a blank node with exactly one value for the property sh:intersection. Intersection expression is a well-formed SHACL list with at least two members.";
 	private String ruleDescriptionUnion = "Union expression is a blank node with exactly one value for the property sh:union. Union expression is a well-formed SHACL list with at least two members.";
@@ -25,9 +30,41 @@ public class IntersectionUnionListRule extends JenaModelUtils  implements Rules 
 	}
 
 	public void validateRule() {
+		int errorUnion = getValuesProperty(unionProperty, ruleDescriptionUnion);
+		int errorIntersection = getValuesProperty(intersectionProperty, ruleDescriptionIntersection);
+		
+		report.setErrors(errorUnion + errorIntersection);
+	}
+	
+	private int getValuesProperty(String propertyName, String ruleDescription) {
 		//1. Get sh:intersection / sh:union
-		//2. Validate is a list
-		//3. Validate it has at least 2 members
+		int errors = 0;
+		Property pParameter = this.currentModel.getProperty(propertyName);
+		
+		NodeIterator ri = this.currentModel.listObjectsOfProperty(pParameter);
+		
+		while(ri.hasNext()) {
+			RDFNode res = ri.next();
+			
+			if(res.isResource()) {
+				//2. Validate is a list
+				try {
+					RDFList list = this.currentModel.getList(res.asResource());	
+					
+					//3. Validate it has at least 2 members
+					if(list.size()<2) {	
+						errors++;
+						report.setErrorItem(ruleDescription, propertyName, null, null, res.asResource().toString());						
+					}
+				}catch(Exception e){	
+					errors++;
+					report.setErrorItem(ruleDescription, propertyName, null, null, res.asResource().toString());					
+				}
+			}
+			
+		}
+		
+		return errors;
 	}
 	
 }
