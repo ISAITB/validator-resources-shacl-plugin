6b701835f676a35a522f1657ac5c04eb129a457f
Maria <mfontsan@everis.com>
2020-09-10 15:48:23 +0200

    Implementation rule ID: lists

---
+++ src/main/java/eu/europa/ec/itb/shacl/plugin/rules/ListRule.java
@@ -0,0 +1,51 @@
+package eu.europa.ec.itb.shacl.plugin.rules;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFList;
+import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
+
+import eu.europa.ec.itb.shacl.plugin.Report;
+import eu.europa.ec.itb.shacl.plugin.Rules;
+import eu.europa.ec.itb.shacl.plugin.utils.JenaModelUtils;
+
+/**
+ * Rule ID: lists
+ * Rule Definition: https://www.w3.org/TR/shacl/#defaultValue
+ * Rule Description: Avoid large rdf:list due to its innefficiency.
+ * @author mfontsan
+ *
+ */
+public class ListRule extends JenaModelUtils  implements Rules {
+	private static String listUri = rdfNamespace + "List";
+	private int maxSize = 500;
+	
+	private String ruleDescription = "Avoid large rdf:list due to its innefficiency.";
+	
+
+	public ListRule(Model currentModel, Report report) {
+		super(currentModel, report);
+	}
+
+	public void validateRule() {
+		int warnings = 0;
+
+    	Resource listProperty = this.currentModel.getResource(listUri);
+    	
+    	ResIterator itSubjects = this.currentModel.listSubjectsWithProperty(null, this.currentModel.asRDFNode(listProperty.asNode()));
+    	
+    	while(itSubjects.hasNext()) {
+    		Resource subject = itSubjects.next();
+    		
+    		RDFList list = this.currentModel.getList(subject);
+    		
+    		if(list.size()>maxSize) {
+    			warnings++;
+    			report.setWarningItem(ruleDescription, listUri, null, null, this.getMainShape(subject).toString());
+    		}
+    	}
+		    	
+    	report.setWarnings(warnings);    	
+	}
+	
+}
