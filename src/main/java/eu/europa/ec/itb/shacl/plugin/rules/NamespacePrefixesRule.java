3a10ac258763efb5f2265c7332177f275af15cee
Maria <mfontsan@everis.com>
2020-09-14 12:55:38 +0200

    Implemented rule ID: namespace-prefixes (SEMIC)

---
+++ src/main/java/eu/europa/ec/itb/shacl/plugin/rules/NamespacePrefixesRule.java
@@ -0,0 +1,48 @@
+package eu.europa.ec.itb.shacl.plugin.rules;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jena.rdf.model.Model;
+
+import eu.europa.ec.itb.shacl.plugin.Report;
+import eu.europa.ec.itb.shacl.plugin.Rules;
+import eu.europa.ec.itb.shacl.plugin.utils.JenaModelUtils;
+
+/**
+ * Rule ID: namespace-prefixes
+ * Rule Definition: https://joinup.ec.europa.eu/collection/semantic-interoperability-community-semic (ID: 82)
+ * Rule Description: Prefixes for all namespaces SHOULD be defined.
+ * @author mfontsan
+ *
+ */
+public class NamespacePrefixesRule extends JenaModelUtils  implements Rules {	
+	private String ruleDescription = "Prefixes for all namespaces SHOULD be defined.";
+	private static String reportAssertionID = "prefix";
+	
+	public NamespacePrefixesRule(Model currentModel, Report report) {		
+		super(currentModel, report);
+	}
+
+
+	public void validateRule() {
+		int warnings = 0;
+		
+		Map<String, String> mNamespace = this.currentModel.getNsPrefixMap();
+		
+		if(!mNamespace.isEmpty()) {
+			Set<String> setNs = mNamespace.keySet();
+			
+			for(String ns: setNs) {
+				if(StringUtils.isEmpty(ns)) {
+					warnings++;
+					report.setWarningItem(ruleDescription, reportAssertionID, null, null, mNamespace.get(ns));					
+				}
+			}
+		}
+		
+		report.setWarnings(warnings);
+	}
+	
+}
