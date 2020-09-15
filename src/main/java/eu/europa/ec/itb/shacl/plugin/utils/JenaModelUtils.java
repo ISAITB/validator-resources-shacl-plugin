6b6539009c8256e544f104bcd95f64087289ba54
Maria <mfontsan@everis.com>
2020-09-15 15:10:30 +0200

    Add other methods

--- src/main/java/eu/europa/ec/itb/shacl/plugin/utils/JenaModelUtils.java
+++ src/main/java/eu/europa/ec/itb/shacl/plugin/utils/JenaModelUtils.java
@@ -1,11 +1,15 @@
 package eu.europa.ec.itb.shacl.plugin.utils;
 
+import java.util.List;
+
 import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryException;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.NodeIterator;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
 
 import eu.europa.ec.itb.shacl.plugin.Report;
 
@@ -20,6 +24,7 @@ public abstract class JenaModelUtils {
 	
 	protected static String shaclNamespace = "http://www.w3.org/ns/shacl#";
 	protected static String owlNamespace = "http://www.w3.org/2002/07/owl#";
+	protected static String rdfNamespace = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 	
 
 	public JenaModelUtils(Model currentModel, Report report) {
@@ -28,9 +33,13 @@ public abstract class JenaModelUtils {
 	}
 	
 	public NodeIterator getObjectsOfProperty(String property) {
-		Property p = this.currentModel.getProperty(property);
+		Property p = getProperty(property);
 		
         return this.currentModel.listObjectsOfProperty(p);
+	}
+	
+	public Property getProperty(String propertyName) {
+		return this.currentModel.getProperty(propertyName);
 	}
 	
 	/**
@@ -50,6 +59,23 @@ public abstract class JenaModelUtils {
 		}catch(QueryException e) {
 			return null;
 		}		
+	}
+	
+	/**
+	 * Get shape of a resource
+	 * @param resource
+	 * @return
+	 * 		returns org.apache.jena.rdf.model.Resource
+	 */
+	public Resource getMainShape(Resource resource) {
+		RDFNode resourceRDFNode = this.currentModel.asRDFNode(resource.asNode());
+		List<Resource> listSubjects = this.currentModel.listSubjectsWithProperty(null, resourceRDFNode).toList();
+		
+		if(!listSubjects.isEmpty()) {
+			return getMainShape(listSubjects.get(0));
+		}
+		
+		return resource;
 	}
 	
 	public Report getReport() {
